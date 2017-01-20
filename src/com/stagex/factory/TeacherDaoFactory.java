2dc596be0436172ab4f7c0f5a7ee3b95aaa3f4cd
Alex <alexandre.121095@gmail.com>
2017-01-20 17:05:31 +0100

    no message

--- src/com/stagex/factory/TeacherDaoFactory.java
+++ src/com/stagex/factory/TeacherDaoFactory.java
@@ -24,42 +24,26 @@ public class TeacherDaoFactory extends GenericDaoImpl<Teacher> {
 		int a = 0;
 		while (resultat.next()){
 			a=resultat.getInt("avg(salary)");
-			System.out.println("Le salaire moyen des élèves en stage est:"+a);
+			
 		}
 		resultat.close();
 		return a;
 	}
 	
 	//salary 
-	public static void salJob() throws Exception{
+	public static int salJob() throws Exception{
 		DatabaseConnection dbConn = new DatabaseConnection();
 		Connection conn= (Connection) dbConn.getConnection();
 		Statement statement = (Statement) conn.createStatement();
 		ResultSet resultat = statement.executeQuery( "SELECT avg(salary) FROM "
 				+ "stagex.experience;");
-		System.out.println("Le salaire moyen des anciens élèves est:");
+		int a=0;
 		while (resultat.next()){
-			int a=resultat.getInt("avg(salary)");
-			System.out.println(a);
+			a=resultat.getInt("avg(salary)");
 		}
 		resultat.close();
+		return a;
 	}
 	
-	//company name of internship students
-	public static void companyStudents() throws Exception{
-		DatabaseConnection dbConn = new DatabaseConnection();
-		Connection conn= dbConn.getConnection();
-		Statement statement = conn.createStatement();
-		ResultSet resultat = statement.executeQuery("SELECT distinct companyname FROM stagex.company LEFT JOIN stagex.apply ON "
-				+ "stagex.company.companyId=stagex.apply.companyId;");
-		
-		System.out.println("Les compagnies des élèves en stage sont:");
-		
-		while (resultat.next()){
-			String a=resultat.getString("companyname");
-			System.out.println(a);
-		}
-		resultat.close();
-	}
 	
 }
